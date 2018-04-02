9613ee57109ec891e3b6ac3d7c1e0109abb5aa90
Philip Nunoo <phanunoo@gmail.com>
2018-04-02 00:05:11 +0000

    fix ./links

--- db/index.js
+++ db/index.js
@@ -1,7 +1,4 @@
 export { default as Posts } from './posts/collection';
 export { default as PostsTypesEnum } from './posts/enums/types';
-export { default as PostsLinks } from './posts/links';
 export { default as Users } from './users/collection';
-export { default as UsersLinks } from './users/links';
 export { default as Comments } from './comments/collection';
-export { default as CommentsLinks } from './comments/links';
